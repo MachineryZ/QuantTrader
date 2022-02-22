e55191aaa4ee7c0a6de7e30819d8903189548d18
MachineryZ <zhizuozhang@gmail.com>
2022-02-22 20:02:45 +0800

    daily update

--- BackTrader/strategy/future/macd_ema_trend_following.py
+++ BackTrader/strategy/future/macd_ema_trend_following.py
@@ -3,61 +3,71 @@ import backtrader as bt
 import os
 import numpy as np
 import pandas
-
+from backtrader.comminfo import CommissionInfo
+import pyfolio
 
 class MacdEmaTrendFollowStrategy(bt.Strategy):
     params = (
-        ('params1', 10),
+        ('period_me1', 10),
+        ('period_me2', 20),
+        ('period_signal', 9),
+        ('data_index', 0)
     )
 
-    def log(self, txt, dt=None):
+    def log(self, txt, dt=None, doprint=False):
         """Logging function for this strategy"""
         dt = dt or self.datas[0].datetime.date[0]
         print('%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
 
-        self.ema10 = bt.indicators.EMA(self.datas[0], period=10)
-        self.ema20 = bt.indicators.EMA(self.datas[0], period=20)
-        self.macd = bt.indicators.macd(self.datas[0])
+        self.ema10 = bt.indicators.EMA(self.datas[0], period=self.params.period_me1)
+        self.ema20 = bt.indicators.EMA(self.datas[0], period=self.params.period_me2)
+        self.macd = bt.indicators.macd(self.datas[0], period_me1=self.params.period_me1, period_me2=self.params.period_me2)
 
-        # self.dataclose = self.datas[0].close
-        # self.volume = self.datas[0].volume
+        self.bar_num = 0
 
-        # self.order = None
-        # self.buyprice = None
-        # self.buycomm = None
-
-        # self.high_n = bt.indicators.Highest(self.data.high, period=self.params.n)
-        # self.low_n = bt.indicators.Lowest(self.data.low, period=self.params.n)
-
-        # self.rsv = 100 * bt.DivByZero(
-        #     self.data_close - self.low_n, 
-        #     self.high_n - self.low_n,
-        #     zero=None
-        # )
-        # self.K = bt.indicators.EMA(self.rsv, period=self.params.m)
-        # self.D = bt.indicators.EMA(self.K, period=self.params.l)
-        # self.J = self.params.S * self.D - (self.params.S - 1) * self.K
+        self.history = np.zeros((self.data.buflen(), 4))
+        self.history[0, 1] = self.broker.getvalue()
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
             return
         if order.status in [order.Completed]:
             if order.isbuy():
+                self.log("BUY EXECUTED, Size %i, Price: %.2f, Cost: %.2f, Comm: %.5f" %
+                    (   
+                        order.executed.size,
+                        order.executed.price,
+                        order.executed.value,
+                        order.executed.comm
+                    ), 
+                    doprint=True
+                )
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
                 self.bar_executed_close = self.dataclose[0]
             else:
-                pass
+                self.log("SELL EXECUTED, Size %i, Price: %.2f, Cost: %.2f, Comm %.5f" %
+                    (
+                        order.executed.size,
+                        order.executed.price,
+                        order.executed.value,
+                        order.executed.comm
+                    ), 
+                    doprint=True
+                )
             self.bar_executed = len(self)
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            pass
+            self.log("Order Cancled/Margin/Rejected", doprint=True)
+
+        # Write down: no pending order
         self.order = None
     
     def notify_trade(self, trade):
         if not trade.isclosed:
             return
+        self.log("OPERATION PROFIT, GROSS %.2f, NET %.2f" % (trade.pnl, trade.pnlcomm), doprint=True)
 
     def next(self):
         if self.order:
@@ -73,7 +83,20 @@ class MacdEmaTrendFollowStrategy(bt.Strategy):
         commision: 0.0002
 
         """
+        #  
+        if self.order:
+            return
+    
+    def stop(self):
+        self.log("(me1 Period %i) (me2 Period %i) (period_signal Period %i) Ending Value %.2f" %
+            (
+                self.params.period_me1,
+                self.params.period_me2,
+                self.params.period_
+            )
+        )
 
+        
                     
 if __name__ == '__main__':
     # Create cerebro
@@ -87,7 +110,7 @@ if __name__ == '__main__':
         fromdate=datetime.datetime(2009, 3, 30),
         todate=datetime.datetime(2018, 12, 4),
         dtformat="%Y-%m-%d %H:%M:%S",
-        datetime=0,
+        datetime=1,
         open=2,
         high=3,
         low=4,
@@ -102,7 +125,7 @@ if __name__ == '__main__':
 
     cerebro.addsizer(bt.sizers.FixedSize, stake=100)
 
-    cerebro.broker.addcommissioninfo(commission=0.002, margin=0.1, mult=10) 
+    cerebro.broker.setcommission(commission=0.002)
 
     print("Starting Portfolio Value: %.2f" % cerebro.broker.getvalue())
 
