c224a039ceacc3e93a9966340c35e20db76639a8
zzhang <zzhang@mhfunds.com>
2022-03-08 18:03:47 +0800

    company daily update

--- Alphas/naive_genetic_programming.py
+++ Alphas/naive_genetic_programming.py
@@ -7,3 +7,11 @@ from gplearn.fitness import make_fitness
 from sklearn.utils import check_random_state
 from sklearn.model_selection import train_test_split
 
+data = pandas.read_csv('../data/RB9999.csv')
+print(data)
+
+# set start date and end date
+start_date = '2009-03-30'
+end_date = '2009-04-01'
+fields = ['open', 'high', 'low', 'close', 'volume']
+
