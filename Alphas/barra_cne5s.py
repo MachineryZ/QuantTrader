09803e640fe7f57c545e4614e0783b33972f9697
zzhang <zzhang@mhfunds.com>
2022-03-14 14:30:38 +0800

    add barra cne5s factors

--- Alphas/barra_cne5s.py
+++ Alphas/barra_cne5s.py
@@ -50,7 +50,84 @@ def beta():
 def momentum():
     """
     momentum = sum_{t=21}^525 w_t(ln(1 + r_t) - ln(1 + r_ft))
+
     w_t is the exponential weight
     half life is 126
     """
+    pass
 
+def size():
+    """
+    size = ln capitalization
+    """
+    pass
+
+def earnyild():
+    """
+    earnyild = 0.68 * epibs + 0.11 * etop + 0.21 * cetop
+
+    epibs: analyst's predict ep(earning to price)
+    etop: ttm-ep, 12-months profit / capitalization
+    cetop: 12-months cash flow / capitalization
+    """
+    pass
+
+def resvol():
+    """
+    Residual Volatility:
+    resvol = 0.74 * dastd + 0.16 * cmra + 0.1 * hsigma
+    
+    dastd: 252 trading days extra return weighted volatility, half-life 42
+    cmra: 12 months highest return minus lowest return
+    hsigma: beta residual, 252 trading days, half-life 63
+    """
+    pass
+
+def growth():
+    """
+    growth = 0.47 + sgro + 0.24 * egro + 0.18 * egibs + 0.11 * egibs_s
+
+    sgro: 5 years sale growth rate, (g1 + g2 + g3 + g4 + g5)/5
+    egro: 5 years earning growth rate, (e1 + e2 + e3 + e4 + e5)/5
+    egibs: earning growth in long period
+    egibs_s: earning growth in short period
+    """
+    pass
+
+def bp():
+    """
+    book-to-price
+    """
+    pass
+
+def leverage():
+    """
+    leverage = 0.38 * mlev + 0.35 * dtoa + 0.27 * blev
+
+    mlev: market capitalization leverage
+    dtoa: total debt / total asset
+    belv: net asset + priority stock capitalization + long-term debt / net asset
+    """
+    pass
+
+def liquidity():
+    """
+    liquidty = 0.35 * stom + 0.35 * stoq + 0.3 * stoa
+    
+    stom: turnover of month ST(1)
+    stoq: turnover of 3 months ST(3)
+    stoa: turnover of 12 months ST(12)
+    v_t: volume in t
+    s_t: shares in t
+    ST(T) = ln(1/T sum_{t=1}^{21T} V_t / S_t)
+    """
+    pass
+
+def sizenl():
+    """
+    non-linear size: size ** 3
+    1. size weight sum
+    2. winsorize
+    3. std
+    """
+    pass
